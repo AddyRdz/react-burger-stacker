9d7cba4662a5886276738c70c54aa01da0cb8e8a
HenryH <henry.hong@generalassemb.ly>
2022-02-03 18:35:45 -0800

    Modify starter code to be functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Burger Stacker</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Burger Stacker</h1>
+    </div>
+  )
 }
 
 export default App;
